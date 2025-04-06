507b711f55a1b5ec738b58af6d332314b7aa610e
nazor1k <nazarkorobchuk08@gmail.com>
2025-04-06 16:09:18 +0300

    Add main page component and theme styles; update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
+import {MainPageComponent} from "./pages/main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
