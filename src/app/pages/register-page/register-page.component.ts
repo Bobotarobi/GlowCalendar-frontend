f58202c59690ff6f4ecfe4c9dd35a0126975fe7f
ilyak <ilyakoltsovsky@gmail.com>
2025-04-07 15:35:07 +0300

    Made register methods for service

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -5,6 +5,11 @@ import {MatInput, MatInputModule} from "@angular/material/input";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatAnchor, MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {RegisterService} from "../../services/register/register.service";
+import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {merge} from "rxjs";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -26,8 +31,59 @@ import {MatIcon} from "@angular/material/icon";
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+    registerForm: FormGroup = new FormGroup({
+      code: new FormControl("",
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)]),
+      email: new FormControl("",
+        {
+          validators: [Validators.required, Validators.email],
+          updateOn: 'blur'
+        }
+      ),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl("", [Validators.required, Validators.minLength(6)]),
+    });
     hidePassword: boolean = true;
     hideConfirmPassword: boolean = true;
+
+
+    constructor(
+      private RegisterService: RegisterService,
+      private router: Router,
+      private authentificateService: AuthenticationService
+    ) {
+      const Email = this.registerForm.controls["email"];
+      const Password = this.registerForm.controls["password"];
+      const Code = this.registerForm.controls["code"];
+      const ConfirmPassword = this.registerForm.controls["confirmPassword"];
+      if (Email && Password && Code && ConfirmPassword) {
+        merge(Email.events).pipe(takeUntilDestroyed()).subscribe(() => {});
+        merge(ConfirmPassword.events).pipe(takeUntilDestroyed()).subscribe(() => {});
+        merge(Password.events).pipe(takeUntilDestroyed()).subscribe(() => {});
+        merge(Code.events).pipe(takeUntilDestroyed()).subscribe(() => {});
+      } else{
+        throw new Error("Form controls are not valid");
+      }
+    }
+
+    onSubmit() {
+      if (this.registerForm.invalid){
+        this.registerForm.markAsTouched();
+        return;
+      }
+      const {email, password, confirmPassword, code} = this.registerForm.controls;
+      this.RegisterService.registerCode({
+        code: code,
+        email: email,
+        password: password,
+      }).then(id => {
+        if (id) {
+          this.router.navigate(['/'])
+        } else {
+
+        }
+      });
+    }
 
     passwordClickEvent(event: MouseEvent, buttonType: string): void {
       if (buttonType == "password") {
@@ -37,4 +93,7 @@ export class RegisterPageComponent {
       }
       event.stopPropagation();
     }
+
+  protected readonly onsubmit = onsubmit;
 }
+
